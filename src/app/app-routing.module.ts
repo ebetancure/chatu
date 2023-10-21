4872f9c7d085fca9397c7a806a593c0c0f0da514
Alejandro Arango <alejandroagarcia19@gmail.com>
2023-10-21 14:26:44 -0500

    pagina principal despues de iniciar sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
 import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
+import { PaginaPComponent } from './components/pagina-p/pagina-p.component';
 
 const routes: Routes = [
   { path: '', component: InicioSesionComponent },
   { path: 'registro-usuario', component: RegistroUsuarioComponent },
   { path: 'perfil', component: PerfilComponent },
+  { path: 'pagina-p', component: PaginaPComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
